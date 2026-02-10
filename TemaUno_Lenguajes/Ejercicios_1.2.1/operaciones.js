cd9983a2d1e3be49217f9290fa727a0f066f309f
frederickgabriel <133833699+frederickgabriel@users.noreply.github.com>
2026-02-10 10:07:44 -0600

    Agregando ejercicios tema 1

---
+++ TemaUno_Lenguajes/Ejercicios_1.2.1/operaciones.js
@@ -0,0 +1,24 @@
+// Funciones matemáticas
+function suma(a, b) {
+    return a + b;
+}
+
+function resta(a, b) {
+    return a - b;
+}
+
+function multiplicacion(a, b) {
+    return a * b;
+}
+
+function division(a, b) {
+    return a / b;
+}
+
+// Exportar las funciones
+module.exports = {
+    suma,
+    resta,
+    multiplicacion,
+    division
+};
